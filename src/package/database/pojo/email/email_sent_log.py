a5323eeba48a86bbee68dd3de46458911398f520
MuskStart <138420547+MuskStark@users.noreply.github.com>
2025-09-15 23:00:54 +0800

    :sparkles: Record email sending logs after sending the email

--- src/package/database/pojo/email/email_sent_log.py
+++ src/package/database/pojo/email/email_sent_log.py
@@ -1,14 +1,15 @@
-from distutils.text_file import TextFile
+from peewee import BooleanField, CharField
 
 from ....database.pojo.pojo import PojoBase
 
 
 class EmailSentLog(PojoBase):
-    to = TextFile()
-    cc = TextFile()
-    body = TextFile()
-    subject = TextFile()
-    attachment = TextFile()
+    to = CharField(max_length=500)
+    cc = CharField(max_length=500)
+    body = CharField(max_length=500)
+    subject = CharField(max_length=500)
+    attachment = CharField(max_length=500)
+    is_success = BooleanField(null=True)
 
     class Meta:
         db_table = 'email_sent_log'
